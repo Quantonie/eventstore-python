5e40378509e3065dc0b332911cebf3fc6d65d545
quantonie <antonie@quantsolutions.co.za>
2019-07-17 14:08:17 +0200

    fixup! Add Order model

--- Order.py
+++ Order.py
@@ -6,5 +6,4 @@ class Order:
     def set_status(self, new_status: str):
         if new_status not in ('new', 'paid', 'confirmed', 'shipped'):
             raise ValueError(f'{new_status} is not a correct status!')
-
         self.status = new_status
